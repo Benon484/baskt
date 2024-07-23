f1ae4a84d1e8d99617c5381951ea0b989a174697
Benon .M <141640190+Benon484@users.noreply.github.com>
2024-07-23 21:17:53 +0200

    Add files via upload

---
+++ THE DATA/index.js
@@ -0,0 +1,45 @@
+let count1 = 0
+let count2 = 0
+
+function AddOne(){
+let love = document.getElementById("HOME")
+count1 += 1
+love.textContent = count1
+
+}
+
+function AddTwo(){
+    let love = document.getElementById("HOME")
+    count1 += 2
+    love.textContent = count1
+    
+    }
+
+ function AddThree(){
+        let love = document.getElementById("HOME")
+        count1 += 3
+        love.textContent = count1
+        
+     }
+
+
+     function AddOneGuest(){
+        let love = document.getElementById("GUEST")
+        count2 += 1
+        love.textContent = count2
+        
+        }
+        
+        function AddTwoGuest(){
+            let love = document.getElementById("GUEST")
+            count2 += 2
+            love.textContent = count2
+            
+            }
+        
+         function AddThreeGuest(){
+                let love = document.getElementById("GUEST")
+                count2 += 3
+                love.textContent = count2
+                
+             }
